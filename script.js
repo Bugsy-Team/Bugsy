ec40344e8b37847d8b9a63823dc2a445dea75e17
elisamanzoni <elisa.manzoni@denani.it>
2021-06-15 22:23:54 +0200

    FINAL

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-var container = ($('#cosa_Bugsy').offset().top) / 2;
-
-// var due = ($('#due').offset().top) - container;
-//
-// var tre = ($('#tre').offset().top) - container;
-//
-// var quattro = ($('#quattro').offset().top) - container;
-//
-// var cinque = ($('#cinque').offset().top) - container;
-
-
+var cosa_Bugsy = ($('#cosa_Bugsy').offset().top) / 2;
 
 
 $(".pulsanti_download").each(function(x) {
@@ -21,7 +11,7 @@ $(".pulsanti_download").each(function(x) {
 
 $(document).on('scroll', function() {
 
-  if ($(window).scrollTop() > (container)) {
+  if ($(window).scrollTop() > (cosa_Bugsy)) {
     $("#header").addClass('menu_desktop');
     $("#header").removeClass('menu_desktop_top');
 
@@ -32,62 +22,5 @@ $(document).on('scroll', function() {
 
     console.log("scroll");
   }
-
-
-  // if ($(window).scrollTop() < (due)) {
-  //
-  //   $('#pallino-1').addClass('pallino-active');
-  //   console.log("scroll");
-  // } else {
-  //   $('#pallino-1').removeClass('pallino-active');
-  //   console.log("no");
-  //   console.log($(window).scrollTop());
-  // }
-  //
-  // if ($(window).scrollTop() > (due) && $(window).scrollTop() < (tre)) {
-  //
-  //   $('#pallino-2').addClass('pallino-active');
-  //   console.log("scroll");
-  // } else {
-  //   $('#pallino-2').removeClass('pallino-active');
-  //   console.log("no");
-  //   console.log($(window).scrollTop());
-  // }
-  //
-  //
-  // if ($(window).scrollTop() > (tre) && $(window).scrollTop() < (quattro)) {
-  //
-  //   $('#pallino-3').addClass('pallino-active');
-  //   console.log("scroll");
-  // } else {
-  //   $('#pallino-3').removeClass('pallino-active');
-  //   console.log("no");
-  //   console.log($(window).scrollTop());
-  // }
-  //
-  // if ($(window).scrollTop() > (quattro) && $(window).scrollTop() < (cinque)) {
-  //
-  //   $('#pallino-4').addClass('pallino-active');
-  //   console.log("scroll");
-  // } else {
-  //   $('#pallino-4').removeClass('pallino-active');
-  //   console.log("no");
-  //   console.log($(window).scrollTop());
-  // }
-  //
-  // if ($(window).scrollTop() > (cinque)) {
-  //
-  //   $('#pallino-5').addClass('pallino-active');
-  //   console.log("scroll");
-  // } else {
-  //   $('#pallino-5').removeClass('pallino-active');
-  //   console.log("no");
-  //   console.log($(window).scrollTop());
-  // }
-
-
-
-
-
 
 });
